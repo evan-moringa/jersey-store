c422fd8c8e5216558fdccfe9d71bcc59fc4a2460
Evan Keya <evan.keya@student.moringaschool.com>
2023-11-10 16:42:10 +0300

    project

--- src/component/TeamSearch.js
+++ src/component/TeamSearch.js
@@ -2,53 +2,42 @@ import React, { useState, useEffect } from 'react';
 
 const TeamSearch = () => {
   const [teams, setTeams] = useState([]);
-  const [newTeamName, setNewTeamName] = useState('');
+  const [filteredTeams, setFilteredTeams] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
     fetch('http://localhost:3000/teams')
       .then((response) => response.json())
-      .then((data) => setTeams(data));
+      .then((data) => {
+        setTeams(data);
+        setFilteredTeams(data); // Initialize filteredTeams with all teams
+      });
   }, []);
 
-  const addNewTeam = () => {
-    if (newTeamName) {
-      fetch('http://localhost:3000/teams', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          name: newTeamName,
-        }),
-      })
-        .then((response) => response.json())
-        .then((data) => setTeams((prevTeams) => [...prevTeams, data]));
-
-      setNewTeamName('');
-    } else {
-      alert('Please enter team name.');
-    }
+  const handleSearch = (searchTerm) => {
+    setSearchTerm(searchTerm);
+    const filtered = teams.filter((team) =>
+      team.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setFilteredTeams(filtered);
   };
 
   return (
     <div>
       <h2>Team Collection</h2>
+      <label htmlFor="searchTeam">Search Team:</label>
+      <input
+        type="text"
+        id="searchTeam"
+        value={searchTerm}
+        onChange={(e) => handleSearch(e.target.value)}
+      />
+
       <ul>
-        {teams.map((team) => (
+        {filteredTeams.map((team) => (
           <li key={team.id}>{team.name}</li>
         ))}
       </ul>
-
-      <h2>Add a New Team</h2>
-      <label htmlFor="newTeamName">Team Name:</label>
-      <input
-        type="text"
-        id="newTeamName"
-        value={newTeamName}
-        onChange={(e) => setNewTeamName(e.target.value)}
-      />
-
-      <button onClick={addNewTeam}>Add New Team</button>
     </div>
   );
 };
