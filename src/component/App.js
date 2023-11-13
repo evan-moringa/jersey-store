6efd576bd9505f8d3d56212b2c5a0e09fe281cfa
Evan Keya <evan.keya@student.moringaschool.com>
2023-11-13 15:26:08 +0300

    done

--- src/component/App.js
+++ src/component/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import NavBar from './component/NavBar'
-import Header from './component/Header'
+import NavBar from './NavBar'
+import Header from './Header'
 //import About from './component/About'
 //import TeamList from './component/TeamList'
-import TeamCollection from './component/TeamCollection'
-import Footer from './component/Footer'
+import TeamCollection from './TeamCollection'
+import Footer from './Footer'
 import './App.css'
 export default function App() {
   return (
